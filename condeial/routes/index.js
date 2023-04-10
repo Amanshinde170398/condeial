23660451adfd9587e7f18abd6b4ad22630317b4e
Aman Shinde <1703amanshinde98@gmail.com>
2023-04-10 22:52:47 +0530

    Add home and user controller

--- condeial/routes/index.js
+++ condeial/routes/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-console.log("router loaded");
+const homeController = require("../controllers/home_controller");
+
+// Adding root routes
+router.get("/", homeController.home);
+router.get("/about", homeController.about);
+
+// Adding route for users
+router.use("/users", require("./users"));
 
 module.exports = router;
