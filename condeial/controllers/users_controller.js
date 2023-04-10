42a2379ea88b1211bc0ff18b089aeeec17925fd1
Aman Shinde <1703amanshinde98@gmail.com>
2023-04-10 23:20:40 +0530

    Add more routes and action

--- condeial/controllers/users_controller.js
+++ condeial/controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function (req, res) {
   return res.end("<h1>User Profile</h1>");
 };
+
+module.exports.details = function (req, res) {
+  return res.end("<h1>User Details</h1>");
+};
